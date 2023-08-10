2f4edf6f23f10ab4053afb247dcb8c2faa2595b9
Auto Logon <autologon@CSED.LAB>
2023-08-09 21:11:59 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Teste</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+
+    <header>
+        <nav>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+
+            <h1>Hello World!</h1>
+            
+        </section>
+    </main>
+    <footer>
+
+
+    </footer>
+
+    </body>
+
+</html>
